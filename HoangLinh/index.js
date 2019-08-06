7c7f3e37150333bfa22c844f5c9d280f98bf11de
TnhLinh <tl28052000@gmail.com>
2019-08-07 01:35:29 +0700

    [Linh] commit

---
+++ HoangLinh/index.js
@@ -0,0 +1,22 @@
+
+
+function next() {
+	var crux = document.getElementById("crux");
+	var modelcrux = document.getElementById("modelcrux");
+	var gunmetal = document.getElementById("gunmetal");	
+	var modelgunmetal = document.getElementById("modelgunmetal");
+	if (crux.style.display === "none" && modelcrux.style.display === "none" && gunmetal.style.display === "block" && modelgunmetal.style.display==="block"){
+		crux.style.display = "block";
+		modelcrux.style.display = "block";
+		gunmetal.style.display = "none";
+		modelgunmetal.style.display = "none";
+
+	}
+	else {
+		crux.style.display = "none";
+		modelcrux.style.display = "none";
+		gunmetal.style.display = "block";
+		modelgunmetal.style.display = "block";
+	}
+}
+setInterval(next, 3000);
